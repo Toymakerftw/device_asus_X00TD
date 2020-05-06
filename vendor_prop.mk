83931f5b8711e040a27bbe57af5061ff4c9b719d
SayanAcharya <rowdysayan99@gmail.com>
2020-05-06 04:56:57 +0000

    X00TD: Update following blobs from LA.UM.8.2.r1-06200-sdm660.0
    * ADSP,Alarm,ANT+,Audio,Av,Bluetooth,Charger,CNE,DPM,ESE-powermanager,Factory,FM,Gatekeeper,GPS,Graphics,Keymaster,Listen,Media,Perf,Peripheral Manager,Postprocessing,Sensors Calibration,Soter,Sigma Miracast,Ssgtzd,Thermal,Time services,Touch,TUI,UBWC,Wi-Fi,WFD
    * Added QSEECom Hal blobs
    * Remove some services from init.qcom.rc since they have their own local rc now
    
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: Icd205a36550b29048b2f37c671991e9a1dac2b14

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -255,6 +255,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.atfwd.start=true \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.procedure_bytes=SKIP \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.qti.telephony.vt_cam_interface=1 \
     persist.sys.fflag.override.settings_network_and_internet_v2=true \
