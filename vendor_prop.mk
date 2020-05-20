a03c18c820aa1c1af56a37d4396ff537b795a899
Danish Pathan <38377782+Danish1999@users.noreply.github.com>
2020-05-20 14:34:59 +0530

    X00TD: Update dalvik props from lineage
    * These are more smoother

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -134,12 +134,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik overrides
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.heapgrowthlimit=256m \
+	dalvik.vm.heapgrowthlimit=192m \
 	dalvik.vm.heapstartsize=8m \
 	dalvik.vm.heapsize=512m \
-	dalvik.vm.heaptargetutilization=0.75 \
-	dalvik.vm.heapminfree=512k \
-	dalvik.vm.heapmaxfree=8m
+	dalvik.vm.heaptargetutilization=0.6 \
+	dalvik.vm.heapminfree=8m \
+	dalvik.vm.heapmaxfree=16m
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
