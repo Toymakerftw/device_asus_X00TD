amackpro/device_asus_X00TD-1

X00TD: Add script to apply audio patch
* Run from root directory of rom source

Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

audio_patch.sh
@@ -1,0 +1,3 @@
+cp device/asus/X00T/patches/0001-audio-hal-restore-tfa98xx-logic-for-asus-X00T.patch hardware/qcom-caf/msm8998/audio/tfa.patch
+cd hardware/qcom-caf/msm8998/audio/
+git apply -p1 < tfa.patch

Toymakerftw/device_asus_X00TD

audio_patch.sh
@@ -1,0 +1,3 @@
+cp device/asus/X00TD/patches/0001-audio-hal-restore-tfa98xx-logic-for-asus-X00T.patch hardware/qcom-caf/msm8998/audio/tfa.patch
+cd hardware/qcom-caf/msm8998/audio/
+git apply -p1 < tfa.patch